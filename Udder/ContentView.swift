0853a860f7d535b3aa4efcf055410708c4e94a8a
crpradu <c@mail.com>
2022-01-11 22:00:52 +0530

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by pradeep cr on 11/01/22.
+//  Copyright © 2022 pradeep cr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
